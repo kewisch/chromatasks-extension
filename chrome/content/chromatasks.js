38143bb4f8269dda67c9b1d39b6d19b62cf40e59
Philipp Kewisch <mozilla@kewis.ch>
2020-01-06 11:48:10 +0100

    Import Version 1.0
    Released July 29th, 2013
    Works with Thunderbird 10.0 - 31.*

--- chrome/content/chromatasks.js
+++ chrome/content/chromatasks.js
@@ -1,207 +1,201 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Chromatasks extension code.
- *
- * The Initial Developer of the Original Code is
- *   Philipp Kewisch <mozilla@kewis.ch>
- * Portions created by the Initial Developer are Copyright (C) 2008-2009
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Portions Copyright (C) Philipp Kewisch, 2008-2013 */
 
 "use strict";
 
-var chromatasks = {
-  observer: {
-      QueryInterface: function cMO_QueryInterface(aIID) {
-        if (!aIID.equals(Components.interfaces.calICalendarManagerObserver) &&
-            !aIID.equals(Components.interfaces.calIObserver) &&
-            !aIID.equals(Components.interfaces.nsIObserver) &&
-            !aIID.equals(Components.interfaces.nsISupports)) {
-          throw Components.results.NS_ERROR_NO_INTERFACE;
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://calendar/modules/calUtils.jsm");
+
+(function () {
+  var calendarRuleCache = {};
+  var categoryRuleCache = {};
+
+  var  observer = {
+    QueryInterface: XPCOMUtils.generateQI([
+      Components.interfaces.calICalendarManagerObserver,
+      Components.interfaces.calIObserver,
+      Components.interfaces.nsIObserver,
+    ]),
+
+
+    // calICalendarManagerObserver
+    onCalendarRegistered: function(aCalendar) {
+      updateCalendarColor(aCalendar);
+    },
+
+    onCalendarUnregistering: function(aCalendar) {
+      removeColor(aCalendar.id, calendarRuleCache);
+    },
+
+    onCalendarDeleting: function(aCalendar) {},
+
+    // calIObserver
+    onStartBatch: function() {},
+    onEndBatch: function() {},
+    onLoad: function() {},
+
+    onAddItem: function() {},
+    onModifyItem: function() {},
+    onDeleteItem: function() {},
+    onError: function() {},
+
+    onPropertyChanged: function(aCalendar, aName, aValue, aOldValue) {
+      switch (aName) {
+        case "color":
+            updateCalendarColor(aCalendar);
+            break;
+      }
+    },
+
+    onPropertyDeleting: function() {},
+
+    // nsIObserver
+    observe: function cto_observe(aSubject, aTopic, aData) {
+      try {
+        if (aTopic == "nsPref:changed") {
+          reloadAllRules();
         }
-        return this;
-      },
-      // calICalendarManagerObserver
-      onCalendarRegistered: function cMO_onCalendarRegistered(aCalendar) {
-        chromatasks.updateColor(aCalendar);
-        aCalendar.addObserver(this);
-      },
+      } catch (e) {
+        Components.utils.reportError(e);
+      }
+    },
+  };
 
-      onCalendarUnregistering: function cMO_onCalendarUnregistering(aCalendar) {
-          aCalendar.removeObserver(this);
-          chromatasks.removeRule(aCalendar);
-      },
-
-      onCalendarDeleting: function cMO_onCalendarDeleting(aCalendar) {
-      },
-
-      // calIObserver
-      onStartBatch: function() {},
-      onEndBatch: function() {},
-      onLoad: function() {},
-
-      onAddItem: function() {},
-      onModifyItem: function() {},
-      onDeleteItem: function() {},
-      onError: function() {},
-
-      onPropertyChanged: function cto_onPropertyChanged(aCalendar,
-                                                        aName,
-                                                        aValue,
-                                                        aOldValue) {
-        switch (aName) {
-          case "color":
-              chromatasks.updateColor(aCalendar);
-              break;
+  function getStyleSheet() {
+    if (!getStyleSheet.sheet) {
+      const cssUri = "chrome://chromatasks/content/chromasheet.css";
+      for each (let sheet in document.styleSheets) {
+        if (sheet.href == cssUri) {
+            getStyleSheet.sheet = sheet;
+            break;
         }
-      },
-
-      onPropertyDeleting: function cto_onPropertyDeleted() {},
-
-      observe: function cto_observe(aSubject, aTopic, aValue) {
-          if (aTopic == "nsPref:changed" &&
-              aSubject == "extensions.chromatasks.useBackground") {
-              // We need to update all rules
-              var oldValue = !aValue;
-              var calMgr = getCalendarManager();
-              var cals = calMgr.getCalendars({});
-              for each (var cal in cals) {
-                  chromatasks.removeRule(cal, oldValue);
-                  chromatasks.updateColor(cal, aValue);
-              }
-          }
       }
-  },
-
-  removeRule: function chromatasks_removeRule(aCalendar, aUseBackground) {
-      var sheet = chromatasks.getStyleSheet();
-      if (aUseBackground === undefined) {
-          // If no option was passed, get from prefs
-          aUseBackground = getPrefSafe("extensions.chromatasks.useBackground", false);
-      }
-      var [selector, bareSelector] = chromatasks.compileSelector(aCalendar, aUseBackground);
-      for (var i = 0; i < sheet.cssRules.length; i++) {
-          var thisrule = sheet.cssRules[i];
-          if (thisrule.selectorText == bareSelector) {
-              sheet.deleteRule(i);
-          }
-      }
-
-      // Update the tree
-      var trees = document.getElementsByTagName("calendar-task-tree");
-      for (var i = 0; i < trees.length; i++) {
-        // XXX This doesn't work for some reason, we have to live with that now.
-        var realtree = document.getAnonymousNodes(trees[i])[0];
-        realtree.treeBoxObject.invalidate();
-      }
-  },
-
-  compileSelector: function chromatasks_compileSelector(aCalendar, aUseBackground) {
-      var pseudoclass = (aUseBackground ? "-moz-tree-row" : "-moz-tree-cell-text");
-      var selector = ".calendar-task-tree > treechildren::" + pseudoclass +
-          "(calendarid-" + formatStringForCSSRule(aCalendar.id) + ")";
-      return [selector, selector.replace(/[()]/g, "")];
-  },
-
-  updateColor: function chromatasks_updateColor(aCalendar, aUseBackground) {
-      var sheet = chromatasks.getStyleSheet();
-      if (aUseBackground === undefined) {
-          // If no option was passed, get from prefs
-          aUseBackground = getPrefSafe("extensions.chromatasks.useBackground", false);
-      }
-      var found = false;
-
-      var thisproperty = (aUseBackground ? "background-color" : "color");
-      var otherproperty = (aUseBackground ? "color" : "background-color");
-      var [selector, bareSelector] = chromatasks.compileSelector(aCalendar, aUseBackground);
-
-      for (var i = 0; i < sheet.cssRules.length; i++) {
-          var thisrule = sheet.cssRules[i];
-          if (thisrule.selectorText == bareSelector) {
-              thisrule.style.setProperty(thisproperty,
-                                         aCalendar.getProperty("color") || "#A8C2E1",
-                                         "important");
-              thisrule.style.removeProperty(otherproperty);
-              found = true;
-              break;
-          }
-      }
-
-      if (!found) {
-          var ruleText = selector + "{ " + (aUseBackground ? "background-" : "") + "color: " +
-            (aCalendar.getProperty("color") || "#A8C2E1") + " !important; }";
-          sheet.insertRule(ruleText, sheet.cssRules.length);
-      }
-
-      // Update the tree
-      var trees = document.getElementsByTagName("calendar-task-tree");
-      for (var i = 0; i < trees.length; i++) {
-        // XXX This doesn't work for some reason, we have to live with that now.
-        var realtree = document.getAnonymousNodes(trees[i])[0];
-        realtree.treeBoxObject.invalidate();
-      }
-  },
-
-  load: function chromatasks_load() {
-    window.removeEventListener("load", chromatasks.load, false);
-    var calMgr = getCalendarManager();
-    var cals = calMgr.getCalendars({});
-    calMgr.addObserver(chromatasks.observer);
-    for each (var cal in cals) {
-        cal.addObserver(chromatasks.observer);
-        chromatasks.updateColor(cal);
     }
-  },
-
-  unload: function chromatasks_unload() {
-    window.removeEventListener("unload", chromatasks.unload, false);
-    var calMgr = getCalendarManager();
-    var cals = calMgr.getCalendars({});
-    calMgr.removeObserver(chromatasks.observer);
-    for each (var cal in cals) {
-        cal.removeObserver(chromatasks.observer);
-    }
-  },
-
-  getStyleSheet: function chromatasks_getStyleSheet() {
-    if (!chromatasks.getStyleSheet.sheet) {
-        const cssUri = "chrome://chromatasks/content/chromasheet.css";
-        for each (var sheet in document.styleSheets) {
-            if (sheet.href == cssUri) {
-                chromatasks.getStyleSheet.sheet = sheet;
-                break;
-            }
-        }
-    }
-    return chromatasks.getStyleSheet.sheet;
+    return getStyleSheet.sheet;
   }
-};
 
-window.addEventListener("load", chromatasks.load, false);
-window.addEventListener("unload", chromatasks.unload, false);
+  function reloadAllRules() {
+    // First remove all rules
+    let sheet = getStyleSheet();
+    while (sheet.cssRules.length) {
+      sheet.deleteRule(0);
+    }
+
+    calendarRuleCache = {};
+    categoryRuleCache = {};
+
+    // Update calendar color rules
+    let calMgr = getCalendarManager();
+    let cals = calMgr.getCalendars({});
+    for each (let calendar in cals) {
+      updateCalendarColor(calendar);
+    }
+
+    // Update category color rules
+    let categories = Services.prefs.getChildList("calendar.category.color.");
+    for each (let cat in categories) {
+      var catName = cat.substr(24);
+      updateCategoryColor(catName);
+    }
+  }
+
+  function removeColor(aKey, aCache) {
+    var sheet = getStyleSheet();
+
+    if (aKey in aCache) {
+      let rule = aCache[aKey];
+      for (let i = 0; i < sheet.cssRules.length; i++) {
+        if (sheet.cssRules[i] == rule) {
+          sheet.cssRules.deleteRule(i);
+          delete aCache[aKey];
+        }
+      }
+    }
+
+    updateTree();
+  }
+
+  function updateColor(aSelector, aColor, aKey, aCache, aUseBackground) {
+    let sheet = getStyleSheet();
+    let found = false;
+
+    let thisproperty = (aUseBackground ? "background-color" : "color");
+    let otherproperty = (aUseBackground ? "color" : "background-color");
+
+    if (aKey in aCache) {
+      let rule = aCache[aKey];
+      rule.style.setProperty(thisproperty, aColor, "important");
+      rule.style.removeProperty(otherproperty);
+    } else {
+      let ruleText = aSelector + "{ " + (aUseBackground ? "background-" : "") +
+            "color: " + aColor + " !important; }";
+      let ruleIndex = sheet.insertRule(ruleText, sheet.cssRules.length);
+      let rule = sheet.cssRules[ruleIndex];
+      aCache[aKey] = rule;
+    }
+
+    updateTree();
+  }
+
+  function updateCalendarColor(aCalendar) {
+    if (cal.getPrefSafe("extensions.chromatasks.colorOf", "calendar") == "calendar") {
+      let useBackground = cal.getPrefSafe("extensions.chromatasks.useBackground", false);
+      let color = aCalendar.getProperty("color") || "#A8C2E1";
+      let pseudoclass = (useBackground ? "-moz-tree-row" : "-moz-tree-cell-text");
+      let selector = ".calendar-task-tree > treechildren::" + pseudoclass +
+          "(calendarid-" + formatStringForCSSRule(aCalendar.id) + ")";
+      updateColor(selector, color, aCalendar.id, calendarRuleCache, useBackground)
+    }
+  }
+
+  function updateCategoryColor(aCategory) {
+    if (cal.getPrefSafe("extensions.chromatasks.colorOf", "calendar") == "category") {
+      let useBackground = cal.getPrefSafe("extensions.chromatasks.useBackground", false);
+      let color = cal.getPrefSafe("calendar.category.color." + aCategory) || "";
+      let pseudoclass = (useBackground ? "-moz-tree-row" : "-moz-tree-cell-text");
+      let selector = ".calendar-task-tree > treechildren::" + pseudoclass +
+          "(" + aCategory + ")";
+      updateColor(selector, color, aCategory, categoryRuleCache, useBackground);
+    }
+  }
+
+  function updateTree() {
+    // Update the tree
+    let trees = document.getElementsByTagName("calendar-task-tree");
+    for (let i = 0; i < trees.length; i++) {
+      // XXX This doesn't work for some reason, we have to live with that now.
+      let realtree = document.getAnonymousNodes(trees[i])[0];
+      realtree.treeBoxObject.invalidate();
+    }
+  }
+
+  function load() {
+    window.removeEventListener("load", load, false);
+    reloadAllRules();
+
+    let calMgr = getCalendarManager();
+    calMgr.addObserver(observer);
+    calMgr.addCalendarObserver(observer);
+
+    Services.prefs.addObserver("calendar.category.color.", observer, false);
+    Services.prefs.addObserver("extensions.chromatasks.", observer, false);
+  }
+
+  function unload() {
+    window.removeEventListener("unload", unload, false);
+    Services.prefs.removeObserver("extensions.chromatasks.", observer, false);
+
+    let calMgr = getCalendarManager();
+    calMgr.removeObserver(observer);
+    calMgr.removeCalendarObserver(observer);
+
+    Services.prefs.removeObserver("calendar.category.color.", observer);
+    Services.prefs.removeObserver("extensions.chromatasks.", observer);
+  }
+
+  window.addEventListener("load", load, false);
+  window.addEventListener("unload", unload, false);
+})();
